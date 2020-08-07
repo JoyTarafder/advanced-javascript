8cad4f351084ae220ab83483daa2bf2cdc1805e9
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2020-08-07 14:13:38 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,24 @@
+Undefined :
+let pakhi;
+
+function add(num1, num2){
+    console.log(nun1 + num2);
+    return
+}
+
+function add(num1, num2){
+     console.log(nun1, num2);
+}
+
+const premik = {name:"Hank Dud", phone:0124878};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, ,6]
+console.log(ages[11]);
+
+
+
+
